b8e7aa409bf53784daa8bd5ae308470ecdbfca78
Danil Karpenko <81296950+LIMPIX31@users.noreply.github.com>
2025-01-31 15:40:33 +0100

    feat: lvds buffers (#7)
    * feat: lvds buffers
    * chore: update readme

---
+++ cores/lvds/lvds_pkg.sv
@@ -0,0 +1,10 @@
+// Copyright (c) 2025 Sigma Logic
+
+package lvds_pkg;
+
+    typedef struct packed {
+        logic p;
+        logic n;
+    } pair_t;
+
+endpackage : lvds_pkg
--- verilator/lsp.f
+++ verilator/lsp.f
@@ -5,3 +5,4 @@
 --timing
 prim_sim.sv
 cores/simple_pll/simple_pll_pkg.sv
+cores/lvds/lvds_pkg.sv
