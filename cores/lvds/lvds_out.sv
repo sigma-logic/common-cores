0b4578a8b5e9aa372c6a9cc66c91dd0358c2e5d3
Danil Karpenko <limpix31@gmail.com>
2025-01-31 15:39:01 +0100

    feat: lvds buffers

---
+++ cores/lvds/lvds_out.sv
@@ -0,0 +1,30 @@
+// Copyright (c) 2025 Sigma Logic
+`default_nettype none
+
+module lvds_out
+    import lvds_pkg::pair_t;
+#(
+    parameter string Mode = "True"
+) (
+    input logic single,
+
+    output pair_t pair
+);
+
+    generate
+        if (Mode == "True") begin : gen_true_lvds_out
+            TLVDS_OBUF u_true_lvds_obuf (
+                .I (single),
+                .O (pair.p),
+                .OB(pair.n)
+            );
+        end else begin : gen_emulated_lvds_out
+            ELVDS_OBUF u_emulated_lvds_obuf (
+                .I (single),
+                .O (pair.p),
+                .OB(pair.n)
+            );
+        end
+    endgenerate
+
+endmodule : lvds_out
